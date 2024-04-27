d36e8cf3aad2dc3cbf85f89d685c55fb3f5ca511
Thammineni Mohan Raj Kumar <116945231+Mohan2200031661@users.noreply.github.com>
2024-04-27 15:35:32 +0530

    Update paymentForm.tsx

--- src/components/checkout/paymentForm.tsx
+++ src/components/checkout/paymentForm.tsx
@@ -1,6 +1,5 @@
 import { useState, useContext } from "react";
 import CartContext from "../../context/cartContext/cartContext";
-import config from "../../../config";
 import {
   CardNumberElement,
   CardCvcElement,
