a5b1077e3dc364bcf0fbdfc6a394d69266910f29
Thammineni Mohan Raj Kumar <116945231+Mohan2200031661@users.noreply.github.com>
2024-04-27 15:34:24 +0530

    Update carouselItem.tsx

--- src/components/carousel/carouselItem.tsx
+++ src/components/carousel/carouselItem.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { multipleSpeciesStringConverter } from "../helpers";
-import config from "../../../config";
 export const CarouselItem = (pet: CarouselPet) => {
   return (
     <div
