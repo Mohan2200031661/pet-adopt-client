cdf31b30b0cfd37eb91c227988ad5c307064d329
Thammineni Mohan Raj Kumar <116945231+Mohan2200031661@users.noreply.github.com>
2024-04-27 15:35:19 +0530

    Update checkoutPage.tsx

--- src/components/checkout/checkoutPage.tsx
+++ src/components/checkout/checkoutPage.tsx
@@ -1,5 +1,4 @@
 import { StripeComponent } from "./stripeComponent";
-import config from "../../../config";
 export const CheckoutPage = () => {
   return (
     <div className="container mx-auto px-4">
